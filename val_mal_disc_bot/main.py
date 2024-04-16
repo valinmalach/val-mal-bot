9d84e960bc050bb189055614601f57bfc33ac111
Valin Malach <valinmalach@gmail.com>
2024-04-17 01:16:33 +1000

    Skeleton code for basic ping-pong bot

---
+++ val_mal_disc_bot/main.py
@@ -0,0 +1,29 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord!")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == "ping":
+        await message.channel.send("pong")
+
+
+client.run(DISCORD_TOKEN)
