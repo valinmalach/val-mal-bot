d59c4f35890ab51c58b21b68f22eb7a36fb5ebc9
Valin Malach <valinmalach@gmail.com>
2024-10-06 19:30:27 +1100

    completed reactions stuff for roles

--- cogs/events.py
+++ cogs/events.py
@@ -1,14 +1,38 @@
 import discord
 from discord.ext import commands
 
-from send_discord_message import send_discord_message
-
 
 class Events(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.member = discord.Member
         self.guild = discord.guild
+        # Message_id, reaction_emoji, role_name mapping
+        self.message_reaction_role_map = {
+            1291772906841571500: {"✅": "Followers"},  # Rules message
+            1292348915257053367: {  # Ping roles message
+                "📢": "Announcements",
+                "🔴": "Live Alerts",
+                "❗": "Ping Role",
+            },
+            1292349441763971123: {"🔞": "NSFW Access"},  # NSFW Access role message
+            1292349494666465282: {  # Pronouns roles message
+                "🙋‍♂️": "He/Him/They",
+                "🙋‍♀️": "She/Her/They",
+                "🙋": "They/Them",
+                "❓": "Other/Ask for pronouns",
+            },
+            1292350341521739837: {  # Streamer, Gamer, Artist roles message
+                "📽️": "Streamer",
+                "🎮": "Gamer",
+                "🎨": "Artist",
+            },
+            1292357707365351445: {  # DMs Open, Ask to DM, DMs Closed roles message
+                "🟩": "DMs Open",
+                "🟨": "Ask to DM",
+                "🟥": "DMs Closed",
+            },
+        }
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -28,32 +52,32 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        if payload.message_id == 1291772906841571500:  # rules reaction message id
-            guild_id = payload.guild_id
-            guild = discord.utils.find(lambda g: g.id == guild_id, self.bot.guilds)
-
-            if payload.emoji.name == "✅":
-                role = discord.utils.get(guild.roles, name="Followers")
-
-            member = discord.utils.find(
-                lambda m: m.id == payload.user_id, guild.members
-            )
-            if member is not None:
-                await member.add_roles(role)
+        await self._toggle_role(payload, True)
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
-        if payload.message_id == 1291772906841571500:
-            guild_id = payload.guild_id
-            guild = discord.utils.find(lambda g: g.id == guild_id, self.bot.guilds)
+        await self._toggle_role(payload, False)
 
-            if payload.emoji.name == "✅":
-                role = discord.utils.get(guild.roles, name="Followers")
+    async def _toggle_role(self, payload: discord.RawReactionActionEvent, add: bool):
+        guild = discord.utils.find(lambda g: g.id == payload.guild_id, self.bot.guilds)
+        if guild is None:
+            return
 
-            member = discord.utils.find(
-                lambda m: m.id == payload.user_id, guild.members
-            )
-            if member is not None:
+        member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+        if member is None:
+            return
+
+        message_id = payload.message_id
+
+        role = discord.utils.get(
+            guild.roles,
+            name=self.message_reaction_role_map[message_id][payload.emoji.name],
+        )
+
+        if role is not None:
+            if add:
+                await member.add_roles(role)
+            else:
                 await member.remove_roles(role)
 
 
