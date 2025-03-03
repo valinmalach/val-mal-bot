b413c82b92cf11b74988b6ee464eb87827160b03
Valin Malach <valinmalach@gmail.com>
2025-03-03 16:54:01 +0800

    reference code in xata_init and test.py

---
+++ test.py
@@ -0,0 +1,49 @@
+import json
+import os
+
+from atproto import Client
+from dotenv import load_dotenv
+from xata import XataClient
+
+load_dotenv()
+
+BLUESKY_LOGIN = os.getenv("BLUESKY_LOGIN")
+BLUESKY_APP_PASSWORD = os.getenv("BLUESKY_APP_PASSWORD")
+
+at_client = Client()
+at_client.login(BLUESKY_LOGIN, BLUESKY_APP_PASSWORD)
+
+XATA_API_KEY = os.getenv("XATA_API_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+xata_client = XataClient(api_key=XATA_API_KEY, db_url=DATABASE_URL)
+
+
+last_sync_date_time = xata_client.data().query(
+    "bluesky",
+    {"columns": ["date"], "sort": {"date": "desc"}, "page": {"size": 1}},
+)["records"][0]["date"]
+
+# Get all posts, filter by author handle and last sync, and sort by indexed_at
+posts = sorted(
+    [
+        feed.post
+        for feed in at_client.get_author_feed(actor=BLUESKY_LOGIN).feed
+        if feed.post.author.handle == BLUESKY_LOGIN and feed.post.indexed_at > last_sync_date_time
+    ],
+    key=lambda post: post.indexed_at,
+)
+
+# Build a list with each post's id, date, and URL
+posts = [
+    {
+        "id": post.uri.split("/")[-1],
+        "date": post.indexed_at,
+        "url": f"https://fxbsky.app/profile/valinmalach.bsky.social/post/{post.uri.split('/')[-1]}",
+    }
+    for post in posts
+]
+
+for post in posts:
+    post_id = post.pop("id")
+    resp = xata_client.records().insert_with_id("bluesky", post_id, post)
--- xata_db/xata_init.py
+++ xata_db/xata_init.py
@@ -1,16 +1,23 @@
-# import os
-# from datetime import datetime
+import os
+from datetime import datetime
 
-# from dotenv import load_dotenv
-# from pytz import timezone
-# from xata import XataClient
+from atproto import Client
+from dotenv import load_dotenv
+from pytz import timezone
+from xata import XataClient
 
-# load_dotenv()
+load_dotenv()
 
-# XATA_API_KEY = os.getenv("XATA_API_KEY")
-# DATABASE_URL = os.getenv("DATABASE_URL")
+XATA_API_KEY = os.getenv("XATA_API_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-# client = XataClient(api_key=XATA_API_KEY, db_url=DATABASE_URL)
+BLUESKY_LOGIN = os.getenv("BLUESKY_LOGIN")
+BLUESKY_APP_PASSWORD = os.getenv("BLUESKY_APP_PASSWORD")
+
+at_client = Client()
+at_client.login(BLUESKY_LOGIN, BLUESKY_APP_PASSWORD)
+
+client = XataClient(api_key=XATA_API_KEY, db_url=DATABASE_URL)
 
 # users_table_schema = {
 #     "columns": [
@@ -31,8 +38,26 @@
 #     .isoformat()
 # )
 
-# record = {
-#     "username": "valinmalach",
-#     "birthday": date_with_tz,
-# }
-# resp = client.records().insert_with_id("users", "389318636201967628", record)
+# Get profile's posts with pagination, filter by author handle, and sort by indexed_at
+posts = sorted(
+    [
+        feed.post
+        for feed in at_client.get_author_feed(actor=BLUESKY_LOGIN).feed
+        if feed.post.author.handle == BLUESKY_LOGIN
+    ],
+    key=lambda post: post.indexed_at,
+)
+
+# Build a list with each post's id, date, and URL
+posts = [
+    {
+        "id": post.uri.split("/")[-1],
+        "date": post.indexed_at,
+        "url": f"https://fxbsky.app/profile/valinmalach.bsky.social/post/{post.uri.split('/')[-1]}",
+    }
+    for post in posts
+]
+
+for record in posts:
+    record_id = record.pop("id")
+    resp = client.records().insert_with_id("bluesky", record_id, record)
