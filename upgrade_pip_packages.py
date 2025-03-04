cdf99d2222704ca49e460b17f60547477f3d7c3d
Valin Malach <valinmalach@gmail.com>
2025-03-04 16:28:36 +0800

    remove more files that aren't required by prod

--- upgrade_pip_packages.py
+++
@@ -1,6 +0,0 @@
-import importlib.metadata as metadata
-from subprocess import call
-
-# Upgrade packages (including pip)
-packages = list({dist.metadata["Name"] for dist in metadata.distributions()})
-call("python.exe -m pip install --upgrade " + " ".join(packages), shell=True)
