87c71d537adb63733db2d73ed4dcd4520d829eaa
Valin Malach <valinmalach@gmail.com>
2025-03-05 22:20:52 +0800

    forgot to add self to admin cogs

--- cogs/admin.py
+++ cogs/admin.py
@@ -10,7 +10,7 @@ class Admin(Cog):
 
     @app_commands.command(description="Restarts the bot")
     @app_commands.commands.default_permissions(administrator=True)
-    async def restart(interaction: Interaction):
+    async def restart(self, interaction: Interaction):
         await interaction.response.send_message("Restarting...")
         await asyncio.create_subprocess_exec(
             "powershell.exe", "-File", "C:\\val-mal-bot\\restart_bot.ps1"
@@ -18,7 +18,7 @@ class Admin(Cog):
 
     @app_commands.command(description="Deletes all messages in the channel")
     @app_commands.commands.default_permissions(administrator=True)
-    async def nuke(interaction: Interaction):
+    async def nuke(self, interaction: Interaction):
         await interaction.response.send_message("Nuking channel...")
         await interaction.channel.purge(limit=1000000000)
 
