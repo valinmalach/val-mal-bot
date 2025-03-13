0c702e7e5d34bf03f6734a54012fa42e8445d1ce
Valin Malach <valinmalach@gmail.com>
2025-03-13 15:07:32 +0800

    lru_cache static functions

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from discord.ext.commands import Bot
 from xata import XataClient
 
@@ -6,10 +8,12 @@ async def send_discord_message(message: str, bot: Bot, channel_id: int):
     await bot.get_channel(channel_id).send(message)
 
 
+@lru_cache
 def is_leap_year(year: int) -> bool:
     return (year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0)
 
 
+@lru_cache
 def get_next_leap_year(year: int) -> int:
     while not is_leap_year(year):
         year += 1
