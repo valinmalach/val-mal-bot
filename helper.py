451a8a76146246f7864e9cec4f113e0903cc042b
Valin Malach <valinmalach@gmail.com>
2025-03-21 15:35:26 +0800

    add microseconds to get_age and uncache it
    - It is not a static function due to getting the datetime.now

--- helper.py
+++ helper.py
@@ -37,6 +37,41 @@ def update_birthday(
         return False, e
 
 
+def get_age(date_time: datetime) -> str:
+    now = datetime.datetime.now(datetime.timezone.utc)
+    age = relativedelta.relativedelta(now, date_time)
+    years, months, days, hours, minutes, seconds, microseconds = (
+        age.years,
+        age.months,
+        age.days,
+        age.hours,
+        age.minutes,
+        age.seconds,
+        age.microseconds,
+    )
+    seconds += microseconds / 1000000
+    parts = []
+    if years > 0 or months > 0:
+        if years:
+            parts.append(format_unit(years, "year"))
+        if months:
+            parts.append(format_unit(months, "month"))
+        if days:
+            parts.append(format_unit(days, "day"))
+    else:
+        if weeks := days // 7:
+            parts.append(format_unit(weeks, "week"))
+        if days := days % 7:
+            parts.append(format_unit(days, "day"))
+        if hours:
+            parts.append(format_unit(hours, "hr"))
+        if minutes:
+            parts.append(format_unit(minutes, "min"))
+        if seconds or not parts:
+            parts.append(format_unit(seconds, "sec"))
+    return ", ".join(parts)
+
+
 @cache
 def is_leap(year: int) -> bool:
     return (year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0)
@@ -66,37 +101,3 @@ def get_ordinal_suffix(n: int) -> str:
 @cache
 def format_unit(value: int, unit: str) -> str:
     return f"{value} {f"{unit}s" if value != 1 else unit}"
-
-
-@cache
-def get_age(date_time: datetime) -> str:
-    now = datetime.datetime.now(datetime.timezone.utc)
-    age = relativedelta.relativedelta(now, date_time)
-    years, months, days, hours, minutes, seconds = (
-        age.years,
-        age.months,
-        age.days,
-        age.hours,
-        age.minutes,
-        age.seconds,
-    )
-    parts = []
-    if years > 0 or months > 0:
-        if years:
-            parts.append(format_unit(years, "year"))
-        if months:
-            parts.append(format_unit(months, "month"))
-        if days:
-            parts.append(format_unit(days, "day"))
-    else:
-        if weeks := days // 7:
-            parts.append(format_unit(weeks, "week"))
-        if days := days % 7:
-            parts.append(format_unit(days, "day"))
-        if hours:
-            parts.append(format_unit(hours, "hr"))
-        if minutes:
-            parts.append(format_unit(minutes, "min"))
-        if seconds or not parts:
-            parts.append(format_unit(seconds, "sec"))
-    return ", ".join(parts)
