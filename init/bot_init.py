9c2fe36e68e6fb3a3fdb90ec0ce5b834c6f9c7fa
Valin Malach <valinmalach@gmail.com>
2025-06-17 17:38:31 +0800

    Initializes bot and restarts live alerts.
    Sets up the Discord bot with necessary intents and registers persistent views.
    
    Implements functionality to restart live alert tasks on bot startup by fetching alert records from Xata and creating asyncio tasks to update them. This ensures alerts are active even after a bot restart. Includes error handling for Xata connection issues.

--- init/bot_init.py
+++ init/bot_init.py
@@ -1,12 +1,113 @@
+import asyncio
+import logging
+
 import discord
 import sentry_sdk
 from discord import CategoryChannel, ForumChannel
 from discord.abc import PrivateChannel
 from discord.ext.commands import Bot
+from requests.exceptions import ConnectionError
 
 from constants import BOT_ADMIN_CHANNEL, GUILD_ID
+from init import xata_client
+from services import update_alert
 
 MY_GUILD = discord.Object(id=GUILD_ID)
+logger = logging.getLogger(__name__)
+
+
+async def restart_live_alert_tasks() -> None:
+    logger.info("Attempting to restart live alert tasks from Xata.")
+
+    page_num = 1
+    while True:
+        try:
+            logger.info(f"Fetching page {page_num} of live alert records from Xata.")
+            records = xata_client.data().query("live_alerts", {"page": {"size": 200}})
+            logger.info(
+                f"Successfully fetched page {page_num} of live alert records. Found {len(records['records'])} records on this page."
+            )
+            break
+        except ConnectionError as e:
+            logger.error(
+                f"ConnectionError while fetching page {page_num} of live alert records: {e}. Retrying in 1 second."
+            )
+            sentry_sdk.capture_exception(e)
+            await asyncio.sleep(1)
+
+    logger.info(f"Processing {len(records['records'])} records from page {page_num}.")
+    for alert in records["records"]:
+        broadcaster_id = str(alert["id"])
+        channel_id = int(alert["channel_id"])
+        message_id = int(alert["message_id"])
+        stream_id = str(alert["stream_id"])
+        stream_started_at = str(alert["stream_started_at"])
+        logger.info(
+            f"Processing alert: broadcaster_id={broadcaster_id}, channel_id={channel_id}, message_id={message_id}, stream_id={stream_id}, stream_started_at={stream_started_at}"
+        )
+        asyncio.create_task(
+            update_alert(
+                broadcaster_id=broadcaster_id,
+                channel_id=channel_id,
+                message_id=message_id,
+                stream_id=stream_id,
+                stream_started_at=stream_started_at,
+            )
+        )
+        logger.info(
+            f"Created task to update alert for broadcaster_id={broadcaster_id}."
+        )
+
+    while records.has_more_results():
+        page_num += 1
+        while True:
+            try:
+                logger.info(
+                    f"Fetching next page ({page_num}) of live alert records from Xata using cursor: {records.get_cursor()}."
+                )
+                records = xata_client.data().query(
+                    "live_alerts",
+                    {
+                        "page": {"page": {"size": 200}, "after": records.get_cursor()},
+                    },
+                )
+                logger.info(
+                    f"Successfully fetched page {page_num} of live alert records. Found {len(records['records'])} records on this page."
+                )
+                break
+            except ConnectionError as e:
+                logger.error(
+                    f"ConnectionError while fetching page {page_num} of live alert records: {e}. Retrying in 1 second."
+                )
+                sentry_sdk.capture_exception(e)
+                await asyncio.sleep(1)
+
+        logger.info(
+            f"Processing {len(records['records'])} records from page {page_num}."
+        )
+        for alert in records["records"]:
+            broadcaster_id = str(alert["id"])
+            channel_id = int(alert["channel_id"])
+            message_id = int(alert["message_id"])
+            stream_id = str(alert["stream_id"])
+            stream_started_at = str(alert["stream_started_at"])
+            logger.info(
+                f"Processing alert: broadcaster_id={broadcaster_id}, channel_id={channel_id}, message_id={message_id}, stream_id={stream_id}, stream_started_at={stream_started_at}"
+            )
+            asyncio.create_task(
+                update_alert(
+                    broadcaster_id=broadcaster_id,
+                    channel_id=channel_id,
+                    message_id=message_id,
+                    stream_id=stream_id,
+                    stream_started_at=stream_started_at,
+                )
+            )
+            logger.info(
+                f"Created task to update alert for broadcaster_id={broadcaster_id}."
+            )
+
+    logger.info("Finished restarting live alert tasks.")
 
 
 class MyBot(Bot):
@@ -42,6 +143,9 @@ bot = MyBot(command_prefix="$", intents=discord.Intents.all())
 @bot.event
 @sentry_sdk.trace()
 async def on_ready() -> None:
+    logger.info("Creating task to restart live alert tasks from Xata.")
+    asyncio.create_task(restart_live_alert_tasks())
+
     channel = bot.get_channel(BOT_ADMIN_CHANNEL)
     if channel is None or isinstance(
         channel, (ForumChannel, CategoryChannel, PrivateChannel)
